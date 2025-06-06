4186f6e589cb86471948fdeb605d628576e770e6
koschkx@gmail.com <96351407+KoschKX@users.noreply.github.com>
2025-06-06 18:08:28 +0200

    fixed navbar on mobile

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+  contactData  = {
+    name: '',
+    email: '',
+    message: ''
+  }
+
+  onSubmit(ngForm: NgForm){
+    if (ngForm.valid && ngForm.submitted) {
+      console.log('Form Submitted!', this.contactData);
+      ngForm.reset(); // Reset the form after submission
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }
